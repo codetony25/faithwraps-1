63f0c22b54ad80ef1c25ca1d4c7bb5568c27ddd9
Nate Brady <nathan.brady@yahoo.com>
2015-09-02 16:33:19 -0700

    Added Manage Orders fucntionality to admin dashboard Cleaned up ajax calls

--- application/models/Order.php
+++ application/models/Order.php
@@ -11,8 +11,8 @@ class Order extends CI_model {
 		parent::__construct();
 	}
 
-	public function get_all_orders() {
-	// Pulls all the orders and joins user, addresses, and all order parts.
+	public function get_order_joined($id) {
+	// Adds the user and address info to an order
 		$product_table = Product::TABLE;
 		$style_table = Product_Style::TABLE;
 		$order_table = self::TABLE;
@@ -21,13 +21,16 @@ class Order extends CI_model {
 		$mailing_address_table = Mailing_Address::TABLE;
 		$billing_address_table = Billing_Address::TABLE;
 
-		// $this->db->select("");
-		$this->db->join($order_table, "$order_part_table.order_id = $order_table.id");
-		$this->db->join($user_table, "$user_table.id = $order_table.user_id");
-		$this->db->join($mailing_address_table, "$mailing_address_table.user_id = $user_table.id");
-		$this->db->join($billing_address_table, "$billing_address_table.user_id = $user_table.id");
-		$this->db->join($style_table, "$style_table.product_id = $order_part_table.product_id");
-		return $this->Order_Part->fetch_all();
+		$this->db->select("$order_table.id, $order_table.user_id, $order_table.total_price, $order_table.shipping, $order_table.tax, $order_table.filled, $order_table.shipped, $order_table.tracking");
+		$this->db->select("$user_table.first_name as user_first_name, $user_table.last_name as user_last_name, $user_table.email as user_email");
+		$this->db->select("$mailing_address_table.first_name as mailing_first_name, $mailing_address_table.last_name as mailing_last_name, $mailing_address_table.address as mailing_address, $mailing_address_table.address_2 as mailing_address_2, $mailing_address_table.city as mailing_city, $mailing_address_table.state as mailing_state, $mailing_address_table.zip_code as mailing_zip_code, $mailing_address_table.country as mailing_country");
+		$this->db->select("$billing_address_table.first_name as billing_first_name, $billing_address_table.last_name as billing_last_name, $billing_address_table.address as billing_address, $billing_address_table.address_2 as billing_address_2, $billing_address_table.city as billing_city, $billing_address_table.state as billing_state, $billing_address_table.zip_code as billing_zip_code, $billing_address_table.country as billing_country");
+		
+		$this->db->join($user_table, "$user_table.id = $order_table.user_id", "left");
+		$this->db->join($mailing_address_table, "$mailing_address_table.user_id = $user_table.id", "left");
+		$this->db->join($billing_address_table, "$billing_address_table.user_id = $user_table.id", "left");
+		
+		return $this->fetch(array($order_table.'.id'=>$id));
 	}
 
 }
--- application/models/Order_Part.php
+++ application/models/Order_Part.php
@@ -11,4 +11,21 @@ class Order_Part extends CI_model {
 		parent::__construct();
 	}
 
+	public function get_order_parts_joined($order_id) {
+	// returns all the order parts associated with an order and joins the
+	// product and style information
+		$product_table = Product::TABLE;
+		$style_table = Product_Style::TABLE;
+		$order_table = Order::TABLE;
+		$order_part_table = self::TABLE;
+
+		$this->db->select("$order_part_table.id, $order_part_table.order_id, $order_part_table.product_id, $order_part_table.product_style_id as style_id");
+		$this->db->select("$product_table.name as product_name");
+		$this->db->select("$style_table.name as style_name, $style_table.image as style_image");
+
+		$this->db->join($style_table, "$style_table.id = $order_part_table.product_style_id", "left");
+		$this->db->join($product_table, "$product_table.id = $style_table.product_id", "left");
+
+		return $this->fetch_all_where(array('order_id'=>$order_id));
+	}
 }
