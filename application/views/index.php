12d078f4de05d4db0995592f063c5c52b4a62558
Tony Nguyen <tonytainguyen25@gmail.com>
2015-09-02 21:09:22 -0700

    Major change to home page, added full width banner, and redefied explore our items

--- application/views/index.php
+++ application/views/index.php
@@ -1,12 +1,1 @@
-
-<div class="products">
-    <?php foreach($products as $product): ?>
-        <div>
-            <a href="/products/<?= $product['id'] ;?>">
-                <img src="/assets/img/products/<?= $product['image']; ?>"  />
-                <h4><?= $product['name']; ?></h4>
-            </a>
-            <p>$<?=$product['price'];?></p>
-        </div>
-    <?php endforeach; ?>
-</div><!-- END OF PRODUCTS -->
+<!-- Masonary Goes here -->
--- application/views/templates/bootstrap.php
+++ application/views/templates/bootstrap.php
@@ -57,9 +57,23 @@
             <header>
                 <div class="maincontent">
                     <h1>Welcome To FaithWraps</h1>
+                    <h3>Style to wrap your beliefs around</h3>
                 </div>
             </header>
-        <?php endif; ?>  
+            <div class="randomScroll">
+                <h2 class="text-center explore">Explore Our Items</h2>
+                <div class="randomProducts">
+                    <?php foreach($products as $product): ?>
+                        <div>
+                            <a href="/products/<?= $product['id'] ;?>">
+                                <img src="/assets/img/products/<?= $product['image']; ?>"  />
+                                <h4><?= $product['name']; ?></h4>
+                            </a>
+                            <p>$<?=$product['price'];?></p>
+                        </div>
+                    <?php endforeach; ?>
+                </div><!-- END OF randomProducts -->
+            <?php endif; ?>  
           
     <div class="container">
 
@@ -118,6 +132,8 @@
     <script src="/assets/js/bootstrap.min.js"></script>
     <script src="/assets/js/vendor/jquery.validate.js"></script>
     <script src="/assets/js/vendor/slick/slick.js"></script>
+    <script src="/assets/js/vendor/masonry/masonry.pkgd.js"></script>
+    <script src="/assets/js/vendor/masonry/masonry.js"></script>
     <script src="/assets/js/plugins.js"></script>
     <script src="/assets/js/main.js"></script>
     <?php
