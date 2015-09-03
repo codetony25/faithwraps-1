73aa242e5d2ea3f27a8fccfb2cafe4d8ca6a7688
Tony Nguyen <tonytainguyen25@gmail.com>
2015-09-02 17:38:27 -0700

    Changes made to pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,43 @@
 $(function(){
 
-	$('.contactinfo').click(function(){
-		$('.contact').slideDown();
-	});
+
+//Main full sized image
+  $.fn.fullBg = function(){
+    var bgImg = $(this);		
+    
+    function resizeImg() {
+      var imgwidth = bgImg.width();
+      var imgheight = bgImg.height();
+			
+      var winwidth = $(window).width();
+      var winheight = $(window).height();
+		
+      var widthratio = winwidth / imgwidth;
+      var heightratio = winheight / imgheight;
+			
+      var widthdiff = heightratio * imgwidth;
+      var heightdiff = widthratio * imgheight;
+		
+      if(heightdiff>winheight) {
+        bgImg.css({
+          width: winwidth+'px',
+          height: heightdiff+'px'
+        });
+      } else {
+        bgImg.css({
+          width: widthdiff+'px',
+          height: winheight+'px'
+        });		
+      }
+    } 
+    resizeImg();
+    $(window).resize(function() {
+      resizeImg();
+    }); 
+  };
+
+  $("#background").fullBg();
+
 
 	
 //Fading in pictures on items page
