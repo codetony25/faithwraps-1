e58f672526e4c76fd79de564fd81fbf0b9f5fa54
Nate Brady <nathan.brady@yahoo.com>
2015-09-04 09:23:36 -0700

    Moved files

---
+++ application/views/infos/about.php
@@ -0,0 +1,13 @@
+<div class="aboutus text-center">
+    <div class="row">
+        <div class="col-sm-6">
+            <img src="/assets/img/info_banner.jpg">
+        </div>
+        <div class="col-sm-6">
+            <h2 class="text-center">About Us</h2>
+            <p>Faith Benoit, owner and designer of FaithWraps, has been long collecting gem stones since the young age of 8 years old. Entranced by their beauty and energy, she has come to intimately know them. She has since studied Native American customs and adapted them into her lifestyle. Faith is now a Reiki Practitioner and charges each stone used in her jewelry.</p>
+            <p>Although she has an apparel design background, she has always had a love of handmade jewelry. Her goal is to create artistic jewelry that's different and original. A piece of art that can effortlessly make a woman feel beautiful and unique. Every piece is carefully created with love and the best intentions. We hope that our jewelry will inspire you and add some warmth to your life. </p>
+            <p>Love and Light, FaithWraps</p>
+        </div>
+    </div>
+</div>
